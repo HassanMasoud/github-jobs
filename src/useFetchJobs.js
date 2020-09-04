7dca77f088ed5daa349c4e73aeb3432d1aad2646
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-04 22:16:59 +0300

    Handle parameter and page changes

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,10 +1,14 @@
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
+import axios from "axios";
 
 const ACTIONS = {
   MAKE_REQUEST: "make-request",
   GET_DATA: "get-data",
   ERROR: "error",
 };
+
+const BASE_URL =
+  "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -28,11 +32,21 @@ function reducer(state, action) {
 }
 
 export default function useFetchJobs(params, page) {
-  const [state, dispactch] = useReducer(reducer, { jobs: [], loading: true });
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
 
-  return {
-    jobs: [],
-    loading: false,
-    error: false,
-  };
+  useEffect(() => {
+    dispatch({ type: ACTIONS.MAKE_REQUEST });
+    axios
+      .get(BASE_URL, {
+        params: { markdown: true, page: page, ...params },
+      })
+      .then((res) => {
+        dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
+      })
+      .catch((e) => {
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
+  }, [params, page]);
+
+  return state;
 }
