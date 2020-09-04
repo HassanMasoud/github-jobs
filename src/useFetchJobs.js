6e1b4e80aa0fafeafd045e0b91cc499971426cbb
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-04 21:56:37 +0300

    Handle state using useReducer

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,7 +1,38 @@
+import { useReducer } from "react";
+
+const ACTIONS = {
+  MAKE_REQUEST: "make-request",
+  GET_DATA: "get-data",
+  ERROR: "error",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      return { loading: true, jobs: [] };
+
+    case ACTIONS.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs };
+
+    case ACTIONS.ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      };
+
+    default:
+      return state;
+  }
+}
+
 export default function useFetchJobs(params, page) {
+  const [state, dispactch] = useReducer(reducer, { jobs: [], loading: true });
+
   return {
-    jobs: [4, 5, 6],
-    loading: true,
-    error: true,
+    jobs: [],
+    loading: false,
+    error: false,
   };
 }
