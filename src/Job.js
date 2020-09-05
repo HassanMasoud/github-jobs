bf20073c7db102fa2a40a84310fc4a2d818d85c9
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-05 15:05:43 +0300

    Add job info

--- src/Job.js
+++ src/Job.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Card, Badge, Button, Collapse } from "react-bootstrap";
+import ReactMarkdown from "react-markdown";
 
 function Job({ job }) {
-  return <div>{job.title}</div>;
+  return (
+    <Card>
+      <Card.Body>
+        <div className="d-flex justify-content-between">
+          <div>
+            <Card.Title>
+              {job.title} -{" "}
+              <span className="text-muted font-weight-light">
+                {job.company}
+              </span>
+            </Card.Title>
+            <Card.Subtitle className="text-muted mb-2">
+              {new Date(job.created_at).toLocaleDateString()}
+            </Card.Subtitle>
+            <Badge variant="secondary" className="mr-2">
+              {job.type}
+            </Badge>
+            <Badge variant="secondary">{job.location}</Badge>
+            <div style={{ wordBreak: "break-all" }}>
+              <ReactMarkdown source={job.how_to_apply} />
+            </div>
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default Job;
